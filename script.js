fdc376985146f0811394d5a5aac7c1e6e849fa8a
Aish <156216275+axshanand@users.noreply.github.com>
2025-08-06 14:51:38 +0530

    Add files via upload
    Updated

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 // TYPEWRITER ANIMATION FOR LANDING PAGE
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var app = document.getElementById('text-container');
     var typewriter = new Typewriter(app, {
         loop: true
     });
 
     typewriter.typeString('Hey<br>')
-    .pauseFor(300)
-    .typeString('I am Aishwarya Anand')
-    .pauseFor(3000)
-    .deleteAll()
-    .pauseFor(1000)
-    .typeString("B.Tech student at IGDTUW")
-    .pauseFor(3000)
-    .start();
+        .pauseFor(300)
+        .typeString('I am Aishwarya Anand')
+        .pauseFor(3000)
+        .deleteAll()
+        .pauseFor(1000)
+        .typeString("B.Tech student at IGDTUW")
+        .pauseFor(3000)
+        .start();
 });
 
-
-
-window.onscroll = function() {
+// HEADER STICKY ON SCROLL
+window.onscroll = function () {
     const header = document.getElementById('header');
     const headerline = document.getElementById('header-line');
     const scrollPosition = window.scrollY;
@@ -32,9 +31,24 @@ window.onscroll = function() {
     }
 };
 
+// SCROLL TO TOP
 function scrollToTop() {
     window.scrollTo({
         top: 0,
         behavior: 'smooth'
     });
 }
+console.log("JS script loaded!");
+
+// === honors SECTION SCROLL-CYCLING ===
+// Reveal honors timeline cards on scroll
+window.addEventListener("scroll", () => {
+    const items = document.querySelectorAll(".timeline-item");
+    items.forEach((item) => {
+      const box = item.getBoundingClientRect();
+      if (box.top < window.innerHeight - 100) {
+        item.classList.add("show");
+      }
+    });
+  });
+  
